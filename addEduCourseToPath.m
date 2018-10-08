20739aa91afc6b06a4dddb457211b422cac554f4
Pavel Roslovets <roslovets@exponenta.ru>
2018-10-08 15:58:27 +0300

    Toolbox started

--- addEduCourseToPath.m
+++ addEduCourseToPath.m
@@ -1,4 +1,11 @@
-function fs = addMLCourseToPath
+function addEduCourseToPath(add)
+if nargin < 1
+    add = true;
+end
 fs = split(genpath(fileparts(mfilename('fullpath'))), ';'); % get folders
 fs = fs(~contains(rmmissing(fs), {'.git' 'work'})); % filter folders
-addpath(fs{:}); % add folders to path
+if add
+    addpath(fs{:}); % add folders to path
+else
+    rmpath(fs{:})
+end
