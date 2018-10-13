353866bb2252798b814b1f1dbacac02314077686
Pavel Roslovets <roslovets@exponenta.ru>
2018-10-13 14:43:01 +0300

    Toolbox updater integrated

--- ToolboxConfig.m
+++
@@ -1,7 +0,0 @@
-function conf = ToolboxConfig()
-conf.name = 'MATLAB Course for Educators';
-conf.pname = 'MATLAB-Course-for-Educators.prj';
-% conf.origin = 'https://github.com/ETMC-Exponenta/MATLAB-Course-for-Educators.git';
-conf.origin = 'https://github.com/ETMC-Exponenta/MATLAB-Course-for-Educators';
-conf.builder = '0.1';
-
--- dev_on.m
+++ dev_on.m
@@ -1,4 +1,5 @@
-dev = Dev
+dev = MATLABCourseforEducatorsUpdater;
 % to deploy run: dev.deploy(v)
+% to build run dev.build
 % to push new version: dev.push
 % to tag new release: dev.tag
